b5c3e60221525b50614ed963b744c8b216ccc51a
William Rivera <wjrivera247@fullsail.edu>
2013-01-24 17:23:16 -0500

    JSON Data

--- Rivera_William_Assignment3.js
+++ Rivera_William_Assignment3.js
@@ -5,5 +5,36 @@
 // Project 3
 // Third Story on JavaScript
 
-// My Global Variables
+// JSON Data
+var json = {
+    "commanders": [
+        {
+            "name": "Ethan M. Rivers",
+            "age": 30,
+            "cruiser": "Blackskull",
+            "access": true
+        },
+        {
+            "name": "Gian Hunts",
+            "age": 26,
+            "cruiser": "Uziel",
+            "access": true
+        },
+        {
+            "name": "Dylan Cages",
+            "age": 22,
+           "cruiser": "Bloodhawk",
+           "access": false
+        }
+    ]
+        
+};
 
+var handleData = function (json) {
+    for (var i = 0; i < json.commanders.length; i++){
+        var commander = json.commanders[i];
+        console.log("This is " + commander.name + " and is " + commander.age + " years old.");
+        console.log("He submit a " + commander.access + " incident reports on his cruiseship " + commander.cruiser + ".");
+    };
+};
+handleData(json);
